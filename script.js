9d9fcaa3aa4fa87c15ff4aa408f9a1c30ef7f6e3
Brian Omondi <107584987+brc0d3s@users.noreply.github.com>
2024-03-03 07:29:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sectionHeadings = document.querySelectorAll(".section-heading");
+    const toggleThemeButton = document.getElementById("toggle-theme");
+    const printButton = document.getElementById("print-btn");
+
+    // Dark mode toggle functionality
+    toggleThemeButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-theme");
+    });
+
+    // Section toggle functionality
+    sectionHeadings.forEach((heading) => {
+        heading.addEventListener("click", function () {
+            this.nextElementSibling.classList.toggle("show-section");
+        });
+    });
+
+    // Print button functionality
+    printButton.addEventListener("click", function () {
+        window.print();
+    });
+});
